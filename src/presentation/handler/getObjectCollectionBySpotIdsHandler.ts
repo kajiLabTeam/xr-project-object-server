fcce14b41dffebbef81db68ead5c7f1409918d29
Yusei Mizutani <yusei4520030401@gmail.com>
2024-03-03 01:51:51 +0900

    add : オブジェクト検索ハンドラの実装

---
+++ src/presentation/handler/getObjectCollectionBySpotIdsHandler.ts
@@ -0,0 +1,73 @@
+import { GetObjectCollectionBySpotIdService } from '../../application/service/getObjectCollectionBySpotIdsService';
+import { ObjectAggregate } from '../../domain/model/object/aggregate';
+import { UserAggregate } from '../../domain/model/user/aggregate';
+import express, { Request, Response } from 'express';
+import { ObjectRepository } from '../../infrastructure/repository/objectRepository';
+import { ObjectCollectionRepositoryImpl } from '../../domain/repository_impl/objectCollectionRepositoryImpl';
+import { ObjectBrowsingLogRepository } from '../../infrastructure/repository/objectBrowsingLogRepository';
+import { ObjectCollectionRepository } from '../../infrastructure/repository/objectCollectionRepository';
+
+export const GetObjectCollectionBySpotIdRouter = express.Router();
+
+type GetObjectCollectionBySpotIdRequest = {
+  userId: string;
+  spotIds: string[];
+};
+
+type ObjectItem = {
+  id: string;
+  posterId: string;
+  spotId: string;
+  viewUrl: string;
+};
+
+type GetObjectCollectionBySpotIdResponse = {
+  objects: ObjectItem[];
+};
+
+const getObjectCollectionBySpotIdService =
+  new GetObjectCollectionBySpotIdService(
+    new ObjectCollectionRepository(),
+    new ObjectBrowsingLogRepository(),
+  );
+
+GetObjectCollectionBySpotIdRouter.post(
+  '/api/object/collection/get',
+  async (
+    req: Request<{}, {}, GetObjectCollectionBySpotIdRequest>,
+    res: Response<GetObjectCollectionBySpotIdResponse | string>,
+  ) => {
+    const requestBody: GetObjectCollectionBySpotIdRequest = req.body;
+
+    const userId = UserAggregate.userIdFromStr(requestBody.userId);
+    const spotIds = ObjectAggregate.spotIdsFromStr(requestBody.spotIds);
+
+    const getObjectCollectionBySpotIdResult =
+      await getObjectCollectionBySpotIdService.run(userId, spotIds);
+    if (getObjectCollectionBySpotIdResult === undefined) {
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    // レスポンスを送信
+    const response: GetObjectCollectionBySpotIdResponse = {
+      objects: getObjectCollectionBySpotIdResult
+        .getObjectsOfPrivateValue()
+        .map((object) => {
+          return {
+            id: object.getIdOfPrivateValue().getIdOfPrivateValue(),
+            posterId: object
+              .getUserIdOfPrivateValue()
+              .getIdOfPrivateValue()
+              .getIdOfPrivateValue(),
+            spotId: object.getSpotIdOfPrivateValue().getIdOfPrivateValue(),
+            viewUrl: object
+              .getSignedUrlOfPrivateValue()
+              .getPreSignedUrlOfPrivateValue(),
+          };
+        }),
+    };
+
+    res.json(response);
+  },
+);
